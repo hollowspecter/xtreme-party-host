78a73ca4c45346b3de923fdf9ba39075bd329b9f
Unknown <m.ray.varkki@gmail.com>
2019-01-26 00:35:19 +0100

    Fog of War Collider script added

---
+++ Assets/Scripts/FowManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class FowManager : MonoBehaviour
+{
+
+   public int playersInside = 0;
+   public bool fogOfWarReveal = false;
+
+   private void Start()
+   {
+      //add as Trigger
+      var boxCollider = gameObject.AddComponent<BoxCollider>();
+      boxCollider.isTrigger = true;
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+      playersInside++;
+   }
+
+   private void OnTriggerExit(Collider other)
+   {
+      playersInside--;
+   }
+
+
+
+   private void Update()
+   {
+      if (playersInside >= 1)
+      {
+         // Reveal room
+         fogOfWarReveal = true;
+         return;
+      }
+      
+      //hide room
+
+      fogOfWarReveal = false;
+
+   }
+   
+}
