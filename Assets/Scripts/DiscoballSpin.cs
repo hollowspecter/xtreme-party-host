ab5fc989f21913fd2fc06e28e6699f4ea20edb54
Mathew <m.ray.varkki@gmail.com>
2019-01-26 16:30:33 +0100

    fixed a script in master lol

--- Assets/Scripts/DiscoballSpin.cs
+++ Assets/Scripts/DiscoballSpin.cs
@@ -5,9 +5,11 @@ public class DiscoballSpin : MonoBehaviour
 {
 	public float spinSpeed = 0.5f;
 
+	private float ySpin;
+
 	void FixedUpdate ()
 	{
-		var ySpin = spinSpeed * Time.deltaTime;
+		ySpin += Time.deltaTime * spinSpeed;
 		this.transform.eulerAngles += new Vector3(0,ySpin,0);
 	}
 }
