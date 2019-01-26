beef1e8c26696e68d902dfdfa2e0ef74aacb11eb
Joscha Wülk <wuelk@hotmail.de>
2019-01-26 10:31:04 +0100

    Begin player actions

---
+++ Assets/Scripts/IInteractable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+
+
+    void StartInteracting();
+
+    // Update is called once per frame
+    void StopInteracting();
+
+    void OnInteractingFinish();
+    void OnInteractingInterrupted();
+}
