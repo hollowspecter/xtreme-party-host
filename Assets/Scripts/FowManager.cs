a20ad04dece613dd16d584e32b754d1485389346
Unknown <m.ray.varkki@gmail.com>
2019-01-26 01:14:00 +0100

    Fog of War system added

--- Assets/Scripts/FowManager.cs
+++ Assets/Scripts/FowManager.cs
@@ -1,46 +1,63 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Unity.Collections;
-using UnityEngine;
+﻿using UnityEngine;
+using DG.Tweening;
 
 [RequireComponent(typeof(BoxCollider))]
 public class FowManager : MonoBehaviour
 {
-
-   public int playersInside = 0;
-   public bool fogOfWarReveal = false;
+   //Animation duration
+   public float revealTime = 0.2f;
+   
+   //Wall for movement and fog for alpha blend
+   public GameObject Wall;
+   public GameObject Fog;
+   
+   
+   private int _playersInside = 0;
+   private Material fogMaterial;
+   private Vector3 _initPos;
 
    private void Start()
    {
       //add as Trigger
       var boxCollider = gameObject.AddComponent<BoxCollider>();
       boxCollider.isTrigger = true;
-   }
 
+      fogMaterial = Fog.GetComponent<Renderer>().material;
+      
+      _initPos = Wall.transform.localPosition;
+
+
+   }
+   
+   //Player entered room
    private void OnTriggerEnter(Collider other)
    {
-      playersInside++;
+      if(!other.CompareTag("Player")) return;
+      _playersInside++;
    }
 
+   //Player left room
    private void OnTriggerExit(Collider other)
    {
-      playersInside--;
+      if(!other.CompareTag("Player")) return;
+      _playersInside--;
+      
    }
-
 
 
    private void Update()
    {
-      if (playersInside >= 1)
+      if (_playersInside >= 1)
       {
          // Reveal room
-         fogOfWarReveal = true;
+         Wall.transform.DOMoveY(-2, revealTime);
+         fogMaterial.DOFade(0, revealTime);
          return;
       }
       
       //hide room
-
-      fogOfWarReveal = false;
+      Wall.transform.DOMove(_initPos, revealTime);
+      fogMaterial.DOFade(1, revealTime);
 
    }
    
