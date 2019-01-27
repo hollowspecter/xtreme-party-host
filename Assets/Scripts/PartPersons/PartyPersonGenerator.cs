299dc1f6a87635b9f906ae4986f191bd14224ad1
Vivien Baguio <hollowspecder@gmail.com>
2019-01-27 12:42:38 +0100

    Write Score and leave Behvaiour

--- Assets/Scripts/PartPersons/PartyPersonGenerator.cs
+++ Assets/Scripts/PartPersons/PartyPersonGenerator.cs
@@ -2,6 +2,6 @@
 
 public abstract class PartyPersonGenerator : ScriptableObject
 {
-	public abstract void PPGEn(Vector3 spawnPoint, float maxStayAmt);
+	public abstract void PPGEn(Transform spawnPoint, float maxStayAmt);
 
 }
