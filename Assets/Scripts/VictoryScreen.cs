835766b642bdb58c2945833737e07880a6017a5b
Mathew <m.ray.varkki@gmail.com>
2019-01-27 14:13:29 +0100

    Adding Victory Screen

---
+++ Assets/Scripts/VictoryScreen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class VictoryScreen : MonoBehaviour
+{
+
+	public TextMeshProUGUI VictoryText;
+	public TextMeshProUGUI Score;
+	public String victory = "Epic Party!";
+	public String fail = "worst Party EVAR!";
+
+	private void Start()
+	{
+		//if score is higher than XXX
+		VictoryText.text = victory;
+		
+		//if score is lower than XXX
+		VictoryText.text = fail;
+
+
+		Score.text = ToString(); //insert score
+
+	}
+	
+	
+}
