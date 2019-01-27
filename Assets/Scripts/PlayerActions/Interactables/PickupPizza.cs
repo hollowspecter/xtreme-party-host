2eee1a897de652ae667339e2483cb55e25a36895
Vivien Baguio <hollowspecder@gmail.com>
2019-01-27 03:59:49 +0100

    added some more stuff

---
+++ Assets/Scripts/PlayerActions/Interactables/PickupPizza.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupPizza : PickupItem
+{
+    protected override void OnInteractingFinish()
+    {
+        base.OnInteractingFinish();
+
+        GetComponent<Pizza>().enabled = true;
+    }
+}
---
+++ Assets/Scripts/PlayerActions/Interactables/Telephone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Telephone : GeneralInteractable {
+
+    [Header("Pizza Delivery")]
+    public Transform spawnPoint;
+    public GameObject deliveryPrefab;
+    public Vector2 minMaxDeliveryDuration = new Vector2(15f, 30f);
+
+    protected bool orderOnTheWay;
+
+    protected override void AlternativeFinish()
+    {
+        base.AlternativeFinish();
+
+        if (orderOnTheWay) return;
+        Invoke("Deliver", Random.Range(minMaxDeliveryDuration.x, minMaxDeliveryDuration.y));
+    }
+
+    protected virtual void Deliver()
+    {
+        Instantiate(deliveryPrefab, spawnPoint.position + Vector3.up * 2f, Quaternion.identity);
+        orderOnTheWay = false;
+    }
+}
